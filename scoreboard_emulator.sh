bcc2fd37f63b49cb470c672198fd8337d8949372
Scott Stackley <scott@cloudhealthtech.com>
2017-05-18 05:40:27 +0000

    remove echos

--- scoreboard_emulator.sh
+++ scoreboard_emulator.sh
@@ -3,7 +3,4 @@ BASEBALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EMULATOR_PYTHON_PATH=$BASEBALL_DIR"/emulator/scoreboard_emulator.py"
 PYTHON=`which python`
 
-echo $EMULATOR_PYTHON_PATH
-echo $PYTHON
-
 $PYTHON $EMULATOR_PYTHON_PATH "${@}"
