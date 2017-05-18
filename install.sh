74949821013f82e37e87f37482886d9d3e9f0582
sjstack <scott.stackley@gmail.com>
2017-05-18 03:44:38 +0000

    add some install stuff from raspbian jessie lite

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ sudo dnf install python
 sudo dnf install gcc
 sudo dnf install redhat-rpm-config
 sudo dnf install python-devel
+sudo apt-get installl python-setuptools
 #sudo dnf install which
 #sudo dnf install screen
+
+sudo easy_install pip
 
 #sudo pip install --upgrade pip
 sudo pip install RPi.GPIO
