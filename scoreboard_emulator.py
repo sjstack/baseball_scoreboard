61dc90cb5cef827436350e12d4824e665b827371
Scott Stackley <scott@cloudhealthtech.com>
2017-05-18 05:07:26 +0000

    Add scoreboard emulator script

---
+++ scoreboard_emulator.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python
+
+import argparse
+
+from scoreboard import Scoreboard
+from virtual_scoreboard import VirtualScoreboard
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser( description = 'Live baseball scoreboard emulator.' )
+    arg_parser.add_argument('--team', action='store', default="Red Sox", type=str, help='')
+    arg_parser.add_argument("-v", "--virtual", help="Run scoreboard in virtual mode.", action="store_true")
+    args = arg_parser.parse_args()
+
+    scoreboard = VirtualScoreboard() if args.virtual else Scoreboard()
+    scoreboard.run()
