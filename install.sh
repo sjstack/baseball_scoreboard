d8c2e4b9c21fcaa16a3f2d50d4ad386be114baf5
sjstack <scott.stackley@gmail.com>
2017-05-17 20:01:44 -0400

    added stuff to install notes

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 # Scrap notes from linux install
 
 sudo dnf install python
+sudo dnf install gcc
+sudo dnf install redhat-rpm-config
+sudo dnf install python-devel
 #sudo dnf install which
+#sudo dnf install screen
 
 #sudo pip install --upgrade pip
+sudo pip install RPi.GPIO
 sudo pip install requests
