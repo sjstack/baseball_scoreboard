77c67b7e3e2b04309ba8ebcad055e906acd09109
Scott Stackley <scott.stackley@gmail.com>
2017-06-11 18:48:02 -0400

    Add debug run option and logging.

--- emulator/scoreboard.py
+++ emulator/scoreboard.py
@@ -1,14 +1,26 @@
+import logger as paul_bunyan
+
+DEFAULT_ARGS = {
+    'debug': False,
+    'virtual': True,
+    'team': 'Red Sox',
+    }
 
 class Scoreboard:
     def __init__( self ):
         print 'init Scoreboard'
         self.mb_running = False
-        
-    def run( self ):
+
+    def run( self, args = DEFAULT_ARGS ):
         self.mb_running = True
-        
         while self.mb_running:
-            self.run_()
+            if args['debug']:
+                self.run_()
+            else:
+                try:
+                    self.run_()
+                except Exception as ex:
+                    paul_bunyan.log_exception( ex )
 
     def run_( self ):
         print "Running Scoreboard"
--- emulator/scoreboard_emulator.py
+++ emulator/scoreboard_emulator.py
@@ -1,15 +1,32 @@
 
 import argparse
+import logger as paul_bunyan
 
 from scoreboard import Scoreboard
 from virtual_scoreboard import VirtualScoreboard
 from physical_scoreboard import PhysicalScoreboard
 
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser( description = 'Live baseball scoreboard emulator.' )
-    arg_parser.add_argument('--team', action='store', default="Red Sox", type=str, help='')
-    arg_parser.add_argument("-v", "--virtual", help="Run scoreboard in virtual mode.", action="store_true")
-    args = arg_parser.parse_args()
+    try:
+        arg_parser = argparse.ArgumentParser( description = 'Live baseball scoreboard emulator.' )
+        arg_parser.add_argument('--team',
+                                action='store',
+                                default="Red Sox",
+                                type=str,
+                                help='')
+        arg_parser.add_argument("-v",
+                                "--virtual",
+                                help="Run scoreboard in virtual mode.",
+                                action="store_true")
+        arg_parser.add_argument("-d",
+                                "--debug",
+                                help="Run scoreboard in debug mode.",
+                                action="store_true")
+        args = arg_parser.parse_args()
 
-    scoreboard = VirtualScoreboard() if args.virtual else PhysicalScoreboard()
-    scoreboard.run()
+        scoreboard = VirtualScoreboard() if args.virtual else PhysicalScoreboard()
+        scoreboard.run( vars( args ) )
+
+    except Exception as ex:
+        paul_bunyan.log_exception( ex )
+        print "Exit from exception: ", ex.message
