9e20c97f812ba58d87b1bd54874067887f7c758e
Scott Stackley <scott.stackley@gmail.com>
2017-06-11 22:49:21 -0400

    Add scoreboard url request and GameData var.

--- emulator/scoreboard.py
+++ emulator/scoreboard.py
@@ -1,20 +1,28 @@
+import time
+import json
+import requests
 import logger as paul_bunyan
 
-DEFAULT_ARGS = {
+from game_data import *
+from datetime import datetime, timedelta
+
+DEFAULT_OPTS = {
     'debug': False,
     'virtual': True,
-    'team': 'Red Sox',
-    }
+    'team': 'Red Sox'
+}
 
 class Scoreboard:
     def __init__( self ):
-        print 'init Scoreboard'
-        self.mb_running = False
+        self.running = False
+        self.game_data = GameData()
 
-    def run( self, args = DEFAULT_ARGS ):
-        self.mb_running = True
-        while self.mb_running:
-            if args['debug']:
+    def run( self, opts = DEFAULT_OPTS ):
+        self.opts     = opts
+        self.running = True
+
+        while self.running:
+            if self.opts['debug']:
                 self.run_()
             else:
                 try:
@@ -23,5 +31,32 @@ class Scoreboard:
                     paul_bunyan.log_exception( ex )
 
     def run_( self ):
-        print "Running Scoreboard"
-        self.mb_running = False
+        self.set_scoreboard_url()
+        self.get_scoreboard_json()
+        self.running = False
+
+    def set_scoreboard_url( self, datetime = None ):
+        if None == datetime:
+            datetime = self.datetime_shift()
+        year  = datetime.year
+        month = datetime.month
+        day   = datetime.day
+
+        self.scoreboard_url = 'http://gd2.mlb.com/components/game/mlb/year_%04i/month_%02i/day_%02i/master_scoreboard.json' % (year, month, day)
+
+    def datetime_shift( self, utc_datetime = datetime.utcnow(), hours_rewind=12 ):
+        return utc_datetime - timedelta( hours=hours_rewind )
+
+    def get_scoreboard_json( self ):
+        response = requests.get( self.scoreboard_url )
+        self.response_json = json.loads( response.content )
+        self.game_json     = self.find_game_by_team( self.opts['team'] )
+
+    def find_game_by_team( self, i_team_name ):
+        games = []
+        for game in self.response_json['data']['games']['game']:
+            if game['home_team_name'] == i_team_name or game['away_team_name'] == i_team_name:
+                games.append( game )
+                if game['status']['status'] == "In Progress":
+                    return game
+        return games[-1]
