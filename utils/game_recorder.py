862c4e437cdd12114030dc19b956d0bcfa45c9ce
Scott Stackley <scott@cloudhealthtech.com>
2018-06-12 05:39:40 +0000

    add shell of game recorder class

---
+++ utils/game_recorder.py
@@ -0,0 +1,9 @@
+class GameRecorder:
+    def __init__( self ):
+        print "Here"
+
+    def __del__( self ):
+        print "There"
+
+    def update_record( self, game_json ):
+        print "Word"
